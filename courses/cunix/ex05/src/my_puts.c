dae768c13c90a887c1cc77eaaa209446a720a171
dshqq <dasha0506@ukr.net>
2021-12-17 21:46:01 +0100

    solution

--- courses/cunix/ex05/src/my_puts.c
+++ courses/cunix/ex05/src/my_puts.c
@@ -4,15 +4,8 @@ int my_puts(const char *s)
 {
 	while(*s != '\0')
 	{
-		if (write(1,s++,1) == '\0')
-		{
-			return '\0';
-		}
-
+		write(1,s++,1);
 	}
-	if (write(1,"\n",1) == '\0')
-	{
-		return '\0';
-	}
+	write(1,"\n",1);
 	return 0;
 }
