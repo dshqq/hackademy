a62203f901cbe69c936433e72ef54d54a48f05dc
dshqq <dasha0506@ukr.net>
2021-12-23 13:33:20 +0100

    solution

--- courses/cunix/ex04/src/my_itoa.c
+++ courses/cunix/ex04/src/my_itoa.c
@@ -19,13 +19,33 @@ int my_pow(int a, int b)
 char *my_itoa(int nmb)
 {
 	char *buff;
-        char *str = (char *)malloc(100*sizeof(char));
-	buff = str;
 	int my_int = nmb;
-	int count = 0;
-	for (; my_int != 1; my_int /= 10)
+	int count = -1;
+	int flag = 0;
+	if (nmb < 0)
+	{
+		flag++;
+		nmb *= -1;
+		my_int *= -1;
+	}
+	else if (nmb == 0)
+	{
+		count = 1;
+	}
+	for (; my_int > 0; my_int /= 10)
 	{
 		count++;
+	}
+	char *str = (char *)malloc(count+flag*sizeof(char));
+	buff = str;
+	if (nmb == 0)
+	{
+		*buff = '0';
+		return str;
+	}
+	if (flag == 1)
+	{
+		*buff++ = '-';
 	}
 	for (; count > -1; count--)
 	{
